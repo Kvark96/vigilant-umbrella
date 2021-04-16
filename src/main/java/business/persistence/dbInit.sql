6ca9cd82690f28b2df0096e87d55de08c7e34b34
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-04-16 11:37:57 +0200

    added Db

--- src/main/java/business/persistence/dbInit.sql
+++ src/main/java/business/persistence/dbInit.sql
@@ -3,24 +3,101 @@
     If so, please substitute `startcode` with whatever your project is called.
  */
 
-CREATE DATABASE  IF NOT EXISTS `startcode`;
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema cupcake
+-- -----------------------------------------------------
 
-USE `startcode`;
+-- -----------------------------------------------------
+-- Schema cupcake
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cupcake` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `cupcake` ;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(90) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `role` varchar(20) NOT NULL DEFAULT 'customer',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+-- -----------------------------------------------------
+-- Table `cupcake`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`users` (
+                                                 `user_id` INT NOT NULL AUTO_INCREMENT,
+                                                 `email` VARCHAR(90) NOT NULL,
+    `password` VARCHAR(45) NOT NULL,
+    `role` VARCHAR(20) NOT NULL DEFAULT 'customer',
+    `balance` DOUBLE NULL,
+    PRIMARY KEY (`user_id`),
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 4
+    DEFAULT CHARACTER SET = latin1;
 
-LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES 
-(1,'barbie@world.dk','jensen','customer'),
-(2,'ken@world.com','jensen','customer'),
-(3,'robin@gotham.com','batman','employee');
-UNLOCK TABLES;
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`bottoms`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`bottoms` (
+                                                   `product_id` INT NOT NULL,
+                                                   `name` VARCHAR(45) NULL,
+    `price` DOUBLE NULL,
+    PRIMARY KEY (`product_id`))
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`toppings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`toppings` (
+                                                    `product_id` INT NOT NULL,
+                                                    `name` VARCHAR(45) NULL,
+    `price` DOUBLE NULL,
+    PRIMARY KEY (`product_id`))
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`orders` (
+                                                  `order_id` INT NOT NULL,
+                                                  `user_id` INT NULL,
+                                                  `created` TIMESTAMP NULL DEFAULT current_timestamp,
+                                                  PRIMARY KEY (`order_id`),
+    INDEX `fk_orders_users1_idx` (`user_id` ASC) VISIBLE,
+    CONSTRAINT `fk_orders_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `cupcake`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`orderline`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`orderline` (
+                                                     `orderline_id` INT NOT NULL,
+                                                     `top_id` INT NULL,
+                                                     `bottom_id` INT NULL,
+                                                     `quantity` INT NULL,
+                                                     `order_id` INT NULL,
+                                                     PRIMARY KEY (`orderline_id`),
+    INDEX `fk_products_bottoms1_idx` (`bottom_id` ASC) VISIBLE,
+    INDEX `fk_products_toppings1_idx` (`top_id` ASC) VISIBLE,
+    INDEX `fk_orderline_orders1_idx` (`order_id` ASC) VISIBLE,
+    CONSTRAINT `fk_products_bottoms1`
+    FOREIGN KEY (`bottom_id`)
+    REFERENCES `cupcake`.`bottoms` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_products_toppings1`
+    FOREIGN KEY (`top_id`)
+    REFERENCES `cupcake`.`toppings` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_orderline_orders1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `cupcake`.`orders` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
 
---
+++ src/main/java/business/persistence/updatedDb.sql
@@ -0,0 +1,110 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema cupcake
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema cupcake
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cupcake` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `cupcake` ;
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(90) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `role` VARCHAR(20) NOT NULL DEFAULT 'customer',
+  `balance` DOUBLE NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `user_id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`bottoms`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`bottoms` (
+  `product_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `price` DOUBLE NULL,
+  PRIMARY KEY (`product_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`toppings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`toppings` (
+  `product_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `price` DOUBLE NULL,
+  PRIMARY KEY (`product_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`orders` (
+  `order_id` INT NOT NULL,
+  `id` INT NOT NULL,
+  `created` TIMESTAMP NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`order_id`),
+  INDEX `fk_orders_users1_idx` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC) VISIBLE,
+  CONSTRAINT `fk_orders_users1`
+    FOREIGN KEY (`id`)
+    REFERENCES `cupcake`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`orderline`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`orderline` (
+  `orderline_id` INT NOT NULL,
+  `top_id` INT NULL,
+  `bottom_id` INT NULL,
+  `quantity` INT NULL,
+  `order_id` INT NULL,
+  PRIMARY KEY (`orderline_id`),
+  INDEX `fk_products_bottoms1_idx` (`bottom_id` ASC) VISIBLE,
+  INDEX `fk_products_toppings1_idx` (`top_id` ASC) VISIBLE,
+  INDEX `fk_orderline_orders1_idx` (`order_id` ASC) VISIBLE,
+  CONSTRAINT `fk_products_bottoms1`
+    FOREIGN KEY (`bottom_id`)
+    REFERENCES `cupcake`.`bottoms` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_products_toppings1`
+    FOREIGN KEY (`top_id`)
+    REFERENCES `cupcake`.`toppings` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orderline_orders1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `cupcake`.`orders` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
