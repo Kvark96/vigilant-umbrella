3616ca0a196bfd89bb36d881c0da5fcacb30b127
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-04-21 09:49:06 +0200

    Bottom and toppings entities added

--- src/main/java/business/persistence/MenuMapper.java
+++ src/main/java/business/persistence/MenuMapper.java
@@ -1,82 +1,79 @@
 package business.persistence;
 
+import business.entities.BottomEntities;
+import business.entities.ToppingEntities;
+
 import java.sql.*;
-
-
-
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuMapper {
-
-
-    private  Database database;
-
-
+    private Database database;
 
     public MenuMapper(Database database) {
         this.database = database;
     }
 
-
-    public void getBottoms() throws SQLException {
-        int product_id = 0;
-        String name ="";
-        Double price = 0.00;
-
-        try(Connection connection = database.connect()) {
+    public List<BottomEntities> getBottoomnEntities() {
+        List<BottomEntities> bottomEntitiesList = new ArrayList<>();
+        try (Connection connection = database.connect()) {
 
             String sql = "select * from cupcake.bottoms";
 
 
-            try (PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-                st.setInt(1, product_id);
-                st.setString(2, name);
-                st.setDouble(3, price);
-                st.executeUpdate();
-                ResultSet rs = st.getGeneratedKeys();
-                rs.next();
-                System.out.println( rs);
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    int product_id = rs.getInt("produc_id");
+                    String name = rs.getString("name");
+                    Double price = rs.getDouble("price");
 
+                    bottomEntitiesList.add(new BottomEntities(product_id, name, price));
+
+
+                }
+                return bottomEntitiesList;
             } catch (SQLException e) {
                 throw new SQLException();
             }
-        }catch (SQLException ex) {
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        }
-
-
-    public  void getTops(){
-
-        int product_id = 0;
-        String name ="";
-        Double price = 0.00;
-
-        try(Connection connection = database.connect()) {
-            String sql = "select * from cupcake.toppings";
-
-
-            try (PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-                st.setInt(1, product_id);
-                st.setString(2, name);
-                st.setDouble(3, price);
-                st.executeUpdate();
-                ResultSet rs = st.getGeneratedKeys();
-                rs.next();
-                System.out.println( rs);
-
-            } catch (SQLException e) {
-                throw new SQLException();
-            }
-        }catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-
     }
 
-}
+
+    public List<ToppingEntities> getToppingEntities() {
+        List<ToppingEntities> toppingEntitiesList = new ArrayList<>();
+        try (Connection connection = database.connect()) {
+
+            String sql = "select * from cupcake.Toppings";
+
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    int product_id = rs.getInt("produc_id");
+                    String name = rs.getString("name");
+                    Double price = rs.getDouble("price");
+
+                    toppingEntitiesList.add(new ToppingEntities(product_id, name, price));
+
+
+                }
+                return toppingEntitiesList;
+            } catch (SQLException e) {
+                throw new SQLException();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+
+
+
+
+
+
 
 
 
--- src/main/java/business/services/menuFacade.java
+++ src/main/java/business/services/menuFacade.java
@@ -1,4 +1,28 @@
 package business.services;
 
+import business.entities.BottomEntities;
+import business.persistence.Database;
+import business.persistence.MenuMapper;
+
+import java.util.List;
+
 public class menuFacade {
+
+
+    private MenuMapper menuMapper;
+
+    public menuFacade(Database database) {
+        this.menuMapper = menuMapper;
+    }
+
+
+    public List<BottomEntities> getBottoomnEntities(){
+        return menuMapper.getBottoomnEntities();
+
+    }
+
+    public List<BottomEntities> getToppingEntities(){
+        return menuMapper.getBottoomnEntities();
+
+    }
 }
