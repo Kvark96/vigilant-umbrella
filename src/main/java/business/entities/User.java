f899fc0f697b089a360ccfc51d17c97fd470171c
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-04-21 12:06:58 +0200

    Merge

--- src/main/java/business/entities/User.java
+++ src/main/java/business/entities/User.java
@@ -3,59 +3,57 @@ package business.entities;
 public class User
 {
 
-    public User(String email, String password, String role, double balance)
-    {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.balance;
-    }
-
-    private int id; // just used to demo retrieval of autogen keys in UserMapper
+      private int id; // just used to demo retrieval of autogen keys in UserMapper
     private String email;
     private String password; // Should be hashed and secured
     private String role;
     private Double balance;
 
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
+    public User(int id, String email, String password, String role, Double balance) {
+        this.id = id;
         this.email = email;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(String password)
-    {
         this.password = password;
-    }
-
-    public String getRole()
-    {
-        return role;
-    }
-
-    public void setRole(String role)
-    {
         this.role = role;
+        this.balance = balance;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public void setId(int id)
-    {
+    public void setId(int id) {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
 }
--- src/main/java/business/persistence/UserMapper.java
+++ src/main/java/business/persistence/UserMapper.java
@@ -20,13 +20,14 @@ public class UserMapper
     {
         try (Connection connection = database.connect())
         {
-            String sql = "INSERT INTO users (email, password, role) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO users (email, password, role, balance) VALUES (?, ?, ?,?)";
 
             try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
             {
                 ps.setString(1, user.getEmail());
                 ps.setString(2, user.getPassword());
                 ps.setString(3, user.getRole());
+                ps.setDouble(4,user.getBalance());
                 ps.executeUpdate();
                 ResultSet ids = ps.getGeneratedKeys();
                 ids.next();
@@ -59,7 +60,7 @@ public class UserMapper
                 {
                     String role = rs.getString("role");
                     int id = rs.getInt("id");
-                    User user = new User(email, password, role);
+                    User user = new User(email, password, role,);
                     user.setId(id);
                     return user;
                 } else
