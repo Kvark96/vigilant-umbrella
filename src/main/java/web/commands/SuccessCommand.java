feb176ac10229f04df9eb063fee8b67334ced8eb
Hejlesen <70164440+HejlesenCPH@users.noreply.github.com>
2021-04-28 11:45:27 +0200

    orderline should work

--- src/main/java/web/commands/SuccessCommand.java
+++ src/main/java/web/commands/SuccessCommand.java
@@ -1,5 +1,6 @@
 package web.commands;
 
+import business.entities.Cupcake;
 import business.entities.Orderline;
 import business.exceptions.UserException;
 import web.FrontController;
@@ -13,43 +14,68 @@ public class SuccessCommand extends CommandProtectedPage{
         super(pageToShow, role);
     }
 
-    private void addToOrders(HttpServletRequest request, HttpServletResponse response){
-        try(Connection connection = FrontController.database.connect()){
+    private void addToOrders(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+        try (Connection connection = FrontController.database.connect()) {
 
             int id = (int) request.getSession().getAttribute("user_id");
 
             // Insert new order into system
             String insertSql = "INSERT INTO cupcake.orders (id) VALUES (?)";
-            try(PreparedStatement ps = connection.prepareStatement(insertSql)){
+            try (PreparedStatement ps = connection.prepareStatement(insertSql)) {
                 ps.setInt(1, id);
                 ps.execute();
 
-            } catch (SQLException se){
+            } catch (SQLException se) {
                 System.out.println("Failed to add to orders = ");
                 System.out.println(se.getMessage());
             }
 
             // Retrieve order_id and set in sessionScope
             String getIDSql = "SELECT order_id FROM cupcake.orders WHERE id = ?";
-            try(PreparedStatement ps = connection.prepareStatement(getIDSql)){
+            try (PreparedStatement ps = connection.prepareStatement(getIDSql)) {
                 ps.setInt(1, id);
                 ResultSet rs = ps.executeQuery();
                 rs.next();
                 request.getSession().setAttribute("new_order_id", rs.getInt("order_id"));
-            } catch (SQLException se){
+            } catch (SQLException se) {
                 System.out.println("Failed to retrieve order_id from database = ");
                 System.out.println(se.getMessage());
             }
 
         }
-        catch(SQLException se){
-            System.out.println("Failed to connect in addToOrders");
-            System.out.println(se.getMessage());
-        }
     }
 
+
+        public void insertOrder (HttpServletRequest request, HttpServletResponse response) {
+
+                try (Connection connection = database.connect()) {
+                    String sql = "INSERT INTO orderline (top_id, bottom_id, quantity, order_id) VALUES {?,?,?,?)";
+
+                    try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                        Orderline orderline = (Orderline) request.getSession().getAttribute("orderline");
+                        orderline.getOrderline();
+
+                        for (Cupcake c:orderline.getOrderline()) {
+                            ps.setInt(1, c.getTopping().getProduct_id());
+                            ps.setInt(2,c.getBottom().getProduct_id() );
+                            ps.setInt(3,c.getQuantity());
+                            ps.setInt(4, (Integer) request.getSession().getAttribute("new_order_id"));
+                        }
+
+                        ps.execute();
+
+                    } catch (SQLException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+
+
+
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response){
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 
         addToOrders(request, response);
 
@@ -59,30 +85,4 @@ public class SuccessCommand extends CommandProtectedPage{
         return pageToShow;
     }
 
-
-    public void insertOrder() throws UserException {
-
-        try (Connection connection = database.connect()) {
-            String sql = "INSERT INTO orderline (top_id, bottom_id, quantity, order_id) VALUES {?,?,?,?)";
-
-            try (PreparedStatement ps = connection.prepareStatement(sql)) {
-
-
-
-                ps. setInt(1,);
-                ps.setInt(2,);
-                ps.setInt(3),;
-                ps.setInt(4,);
-
-
-                ps.executeUpdate();
-
-
-            } catch (SQLException ex) {
-                throw new UserException(ex.getMessage());
-            }
-        } catch (SQLException ex) {
-            throw new UserException(ex.getMessage());
-        }
-    }
 }
